6affc9c4293812b722a64ca9a6215ac003e66ca5
Peter Watts <pwattsmail@gmail.com>
2021-11-04 21:45:04 -0400

    initial commit

---
+++ pages/api/metadata/[...slug].js
@@ -0,0 +1,44 @@
+const axios = require('axios')
+
+export default function handler(req, res) {
+    const { slug } = req.query
+    const contract = slug[0]
+    const tokenId = slug[1]
+    const community = slug[2]
+    const base = process.env.NEXT_PUBLIC_CHAIN_ID == 4
+    ? 'https://rinkeby-api.opensea.io/api/v1/asset'
+    : 'https://api.opensea.io/api/v1/asset';
+    let url = `${base}/${contract}/${tokenId}`
+    axios.get(url).then((response) => {
+        if(response.data) {
+            console.log(response.data)
+            let meta = {
+                "name": response.data.name,
+                "description": response.data.description, 
+                "image": response.data.image_url,
+                "community": community,
+                "collection": {
+                    "id":response.data.collection.slug,
+                    "name": response.data.collection.name,
+                    "description": response.data.collection.description,
+                    "image": response.data.collection.image_url,
+                    "royalty_amount": response.data.collection.dev_seller_fee_basis_points,
+                    "royalty_recipient": response.data.collection.payout_address,
+                    "community": community,
+                },
+                "attributes":response.data.traits.map(trait => {
+                    return {
+                        "category":"Properties",
+                        "key": trait.trait_type,
+                        "value": trait.value
+                    }
+                })
+              }
+            res.status(200).json(meta);
+        } else {
+            res.status(200).json({error: "Not found"});
+        }
+    }).catch((error)=>{
+        res.status(200).json({error: "Unknown error"});
+    })
+  }
