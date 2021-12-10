17026f8339dbeb1fd39fc1440ef977ce4e6805a9
Peter Watts <pwattsmail@gmail.com>
2021-12-10 22:02:17 +1100

    cors

--- pages/api/yaml.js
+++ pages/api/yaml.js
@@ -3,6 +3,18 @@ const axios = require('axios')
 
 export default function handler(req, res) {
 
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+
     let url = `https://indexer-v3-api-production.up.railway.app/swagger.json`
     axios.get(url).then((response) => {
         if(response) {
