5f27d80ec0c8cf7772b8b8c20be79c35da506a59
George Roman <george.roman.99@gmail.com>
2021-12-29 07:33:30 +0200

    Remove redundant token range

--- pages/api/metadata/rinkeby/0x79e2d470f950f2cf78eef41720e8ff2cf4b3cd78/[id].js
+++ pages/api/metadata/rinkeby/0x79e2d470f950f2cf78eef41720e8ff2cf4b3cd78/[id].js
@@ -280,7 +280,6 @@ const getMetadata = (id) => {
       "royaltyRecipient": null,
       "community": "loot",
       "contract": "0x79e2d470f950f2cf78eef41720e8ff2cf4b3cd78",
-      "tokenRange": ["1", "8000"],
       "filters": [
         "Item",
         "Greatness"
