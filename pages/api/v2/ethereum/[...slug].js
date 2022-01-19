d4ae743d4a37787e6f9eadad4e39eaf11de6f6ea
Peter Watts <pwattsmail@gmail.com>
2022-01-20 09:44:27 +1100

    change user agent

--- pages/api/v2/ethereum/[...slug].js
+++ pages/api/v2/ethereum/[...slug].js
@@ -2,7 +2,12 @@ const axios = require('axios')
 
 
 async function getTokens(url) {
-    return axios.get(url,{ headers: { "X-API-KEY": process.env.OPENSEA_APIKEY }, }).then((response) => {
+    return axios.get(url,{ 
+        headers: { 
+            "X-API-KEY": process.env.OPENSEA_APIKEY,
+            "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1" 
+        }, 
+    }).then((response) => {
         if(!response.data) {
             return {error: "Not found"}
         } else {
