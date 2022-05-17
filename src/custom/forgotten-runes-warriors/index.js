2d18f98d7ced6674aa47d7889051ca7edd5aa354
tv3636 <tv3636@gmail.com>
2022-05-16 21:08:07 -0700

    Add custom warriors metadata

---
+++ src/custom/forgotten-runes-warriors/index.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const rank = {
+  Head: 11,
+  Body: 10,
+  Companion: 9,
+  Weapon: 8,
+  Shield: 7,
+  Rune: 6,
+  Background: 5,
+  Affinity: 4,
+  "% Traits in Affinity": 3,
+  "# Traits in Affinity": 2,
+  "# Traits": 1,
+};
+
+export const fetchToken = async (_chainId, { contract, tokenId }) => {
+  return axios
+    .get(`https://portal.forgottenrunes.com/api/warriors/data/${tokenId}`)
+    .then((response) => {
+      const attributes = response.data.attributes.reduce((result, trait) => {
+        const traitType =
+          trait.trait_type.charAt(0).toUpperCase() + trait.trait_type.slice(1);
+        result.push({
+          key: traitType,
+          rank: rank[traitType] ? rank[traitType] : null,
+          value: trait.value,
+          kind: isNaN(trait.value) ? "string" : "number",
+        });
+        return result;
+      }, []);
+
+      return {
+        contract,
+        tokenId,
+        name: response.data.name,
+        imageUrl: response.data.image,
+        attributes,
+      };
+    });
+};
+
+export const fetchContractTokens = (_chainId, contract, continuation) => {
+  const pageSize = 1000;
+  const tokenIdRange = [0, 15999];
+
+  const minTokenId = continuation
+    ? Math.max(continuation, tokenIdRange[0])
+    : tokenIdRange[0];
+  const maxTokenId = continuation
+    ? Math.min(continuation + pageSize, tokenIdRange[1])
+    : tokenIdRange[1];
+
+  const assets = [];
+  for (let tokenId = minTokenId; tokenId <= maxTokenId; tokenId++) {
+    assets.push(fetchToken(_chainId, { contract, tokenId }));
+  }
+
+  return {
+    continuation: maxTokenId === tokenIdRange[1] ? undefined : maxTokenId + 1,
+    metadata,
+  };
+};
