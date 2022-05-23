b9a57d8eb5eca38687fd8358524d39548c5bb1eb
George Roman <george.roman.99@gmail.com>
2022-05-23 10:38:14 +0300

    feat: extend mutant ape yacht club metadata

--- src/extend/index.js
+++ src/extend/index.js
@@ -1,4 +1,5 @@
 import * as adidasOriginals from "./adidas-originals";
+import * as mutantApeYachtClub from "./mutant-ape-yacht-club";
 
 export const extendMetadata = (chainId, metadata) => {
   if (metadata) {
@@ -17,3 +18,6 @@ const extend = {};
 
 // Adidas Originals
 extend["1,0x28472a58a490c5e09a238847f66a68a47cc76f0f"] = adidasOriginals;
+
+// Mutant Ape Yacht Club
+extend["1,0x60e4d786628fea6478f785a6d7e704777c86a7c6"] = mutantApeYachtClub;
---
+++ src/extend/mutant-ape-yacht-club/index.js
@@ -0,0 +1,27 @@
+export const extend = (_chainId, metadata) => {
+  let serumType;
+  let name;
+
+  // M3 apes have no other attributes besides `Name`
+  if (metadata.attributes.length === 1) {
+    serumType = "M3";
+    name = metadata.attributes[0].value;
+  } else {
+    serumType = metadata.attributes[0].value.slice(0, 2);
+    name = `#${metadata.tokenId} (${serumType})`;
+  }
+
+  return {
+    ...metadata,
+    name,
+    attributes: [
+      ...metadata.attributes,
+      {
+        key: "Serum Type",
+        value: serumType,
+        kind: "string",
+        rank: 1,
+      },
+    ],
+  };
+};
