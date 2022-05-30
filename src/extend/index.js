983118e09e0e44427faa479feeeb147628815971
Peter Watts <pwattsmail@gmail.com>
2022-05-30 19:28:40 +1000

    lilnouns

--- src/extend/index.js
+++ src/extend/index.js
@@ -2,6 +2,7 @@ import * as adidasOriginals from "./adidas-originals";
 import * as admitOne from "./admit-one";
 import * as mutantApeYachtClub from "./mutant-ape-yacht-club";
 import * as nouns from "./nouns";
+import * as lilnouns from "./lilnouns";
 
 export const extendCollectionMetadata = async (chainId, metadata) => {
   if (metadata) {
@@ -45,3 +46,4 @@ extend["1,0x60e4d786628fea6478f785a6d7e704777c86a7c6"] = mutantApeYachtClub;
 
 // Nouns
 extend["1,0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03"] = nouns;
+extend["1,0x4b10701bfd7bfedc47d50562b76b436fbb5bdb3b"] = lilnouns;
---
+++ src/extend/lilnouns/index.js
@@ -0,0 +1,38 @@
+import { request, gql } from "graphql-request";
+
+import nouns from "./lilnouns.json";
+
+const capitalizeFirstLetter = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
+export const extend = async (_chainId, metadata) => {
+  const traitMap = ["background", "body", "accessory", "head", "glasses"];
+  const data = await request(
+    "https://api.thegraph.com/subgraphs/name/lilnounsdao/lil-nouns-subgraph",
+    gql`{
+      nouns(where: {id: "${metadata.tokenId}"}) {
+        seed {
+          background
+          body
+          accessory
+          head
+          glasses
+        }
+      }
+    }`
+  );
+
+  const traits = [];
+  for (let i in traitMap) {
+    traits.push({
+      key: capitalizeFirstLetter(traitMap[i]),
+      value: nouns[i][data.nouns[0].seed[traitMap[i]]],
+      kind: "string",
+      rank: 5 - i,
+    });
+  }
+
+  return {
+    ...metadata,
+    attributes: traits,
+  };
+};
