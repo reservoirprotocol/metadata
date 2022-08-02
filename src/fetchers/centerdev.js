e4077d3cffc78a79e26cbc119174844407086f99
Ofir Sudai <ofirsud@gmail.com>
2022-08-02 16:29:44 -0400

    feat: centerdev impl

---
+++ src/fetchers/centerdev.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+import { Contract } from "ethers";
+import { Interface } from "ethers/lib/utils";
+import slugify from "slugify";
+
+import { parse } from "../parsers/centerdev";
+import { getProvider } from "../utils";
+
+const getNetworkName = (chainId) => {
+  let network;
+  if (chainId === 1) {
+    network = "ethereum-mainnet";
+  } else if (chainId === 4) {
+    network = "ethereum-rinkeby";
+  } else if (chainId === 5) {
+    network = "ethereum-goerli";
+  } else if (chainId === 10) {
+    network = "optimism-mainnet";
+  } else if (chainId === 136) {
+    network = "polygon-mainnet";
+  } else {
+    throw new Error("Unsupported chain id");
+  }
+
+  return network;
+};
+
+export const fetchCollection = async (chainId, { contract, tokenId }) => {
+  try {
+    const network = getNetworkName(chainId);
+    const url = `https://api.center.dev/v1/${network}/${contract}`;
+
+    const data = await axios
+      .get(url, {
+        headers: { "X-API-KEY": process.env.CENTERDEV_API_KEY.trim() },
+      })
+      .then((response) => response.data);
+
+    return {
+      id: contract,
+      slug: slugify(data.name, { lower: true }),
+      name: data.name,
+      community: null,
+      metadata: null,
+      royalties: [],
+      contract,
+      tokenIdRange: null,
+      tokenSetId: `contract:${contract}`,
+    };
+  } catch {
+    try {
+      const name = await new Contract(
+        contract,
+        new Interface(["function name() view returns (string)"]),
+        getProvider(chainId)
+      ).name();
+
+      return {
+        id: contract,
+        slug: slugify(name, { lower: true }),
+        name: name,
+        community: null,
+        metadata: null,
+        royalties: [],
+        contract,
+        tokenIdRange: null,
+        tokenSetId: `contract:${contract}`,
+        isFallback: true,
+      };
+    } catch {
+      return null;
+    }
+  }
+};
+
+export const fetchTokens = async (chainId, tokens) => {
+  const network = getNetworkName(chainId);
+  const url = `https://api.center.dev/v1/${network}/assets`;
+
+  const data = await axios
+    .post(url, {
+      assets: tokens.map(({ contract, tokenId }) => { return { Address: contract, TokenID: tokenId } }),
+    },{
+      headers: { "X-API-KEY": process.env.CENTERDEV_API_KEY.trim() },
+    })
+
+    .then((response) => response.data);
+
+  return data.map(parse).filter(Boolean);
+};
+
+export const fetchContractTokens = async (chainId, contract, continuation) => {
+  // TODO: To implement
+  return null;
+};
---
+++ src/parsers/centerdev.js
@@ -0,0 +1,29 @@
+export const parse = (asset) => {
+  let attributes = [];
+
+  if (asset.metadata?.attributes) {
+    attributes = asset.metadata.attributes.map((trait) => ({
+      key: trait.trait_type,
+      value: trait.value,
+      kind: typeof trait.value == "number" ? "number" : "string",
+      rank: 1,
+    }));
+  } else if (asset.metadata?.features) {
+    attributes = Object.entries(asset.metadata.features).map(([key, value]) => ({
+      key,
+      value,
+      kind: typeof value == "number" ? "number" : "string",
+      rank: 1,
+    }));
+  }
+
+  return {
+    contract: asset.address,
+    tokenId: asset.token_id,
+    name: asset.name,
+    description: asset.metadata?.description,
+    imageUrl: asset.metadata?.image.replace(/^(ipfs\:\/\/)/,"https://ipfs.io/ipfs/"),
+    mediaUrl: asset.metadata?.animation_url,
+    attributes,
+  };
+};
