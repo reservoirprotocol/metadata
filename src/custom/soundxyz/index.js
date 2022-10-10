5f6aa2148bcf765bef620c7befbbd3eda75783fc
Idan Peleg <ipeleg@hotmail.com>
2022-10-10 10:58:34 -0400

    feat: update soundxyz collection id structure

--- src/custom/soundxyz/index.js
+++ src/custom/soundxyz/index.js
@@ -62,8 +62,8 @@ export const fetchCollection = async (_chainId, { contract, tokenId }) => {
     const [fundingAddress, royaltyBPS] = await nftContract.royaltyInfo(tokenId, BPS_100);
 
     return {
-        id: `${contract}:${nft.release.id}`,
-        slug: slugify(nft.release.id, { lower: true }),
+        id: `${contract}:soundxyz-${nft.release.id}`,
+        slug: slugify(nft.release.titleSlug, { lower: true }),
         name: nft.release.title,
         community: "sound.xyz",
         metadata: {
@@ -96,7 +96,7 @@ export const fetchToken = async (_chainId, { contract, tokenId }) => {
     );
 
     if (!_.isEmpty(newMetadata)) {
-      newMetadata[0].collection = `${contract}:${nft.release.id}`;
+      newMetadata[0].collection = `${contract}:soundxyz-${nft.release.id}`;
       return newMetadata[0];
     }
   } catch (error) {
