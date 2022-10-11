099246d83d6d5c2942765162d542737a18578696
Idan Peleg <ipeleg@hotmail.com>
2022-10-11 14:56:31 -0400

    feat: added sound xyz log

--- src/extend/soundxyz/index.js
+++ src/extend/soundxyz/index.js
@@ -1,6 +1,7 @@
 import { getContractSlug } from "../../custom/soundxyz";
 import ArtistContracts from "./ArtistContracts.json";
 import ReleaseContracts from "./ReleaseContracts.json";
+import { logger } from "../../logger";
 
 export const extend = async (_chainId, metadata) => {
   try {
@@ -8,6 +9,13 @@ export const extend = async (_chainId, metadata) => {
     metadata.collection = `${metadata.contract}:soundxyz-${nft.release.id}`;
     return { ...metadata };
   } catch (error) {
+    logger.error("soundxyz-fetcher",
+      `fetchCollection error. chainId:${_chainId}, contract:${metadata.contract}, message:${
+        error.message
+      },  status:${error.response?.status}, data:${JSON.stringify(
+        error.response?.data
+      )}`);
+
     throw error
   }
 };
