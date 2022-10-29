dfabfbfcf8142f24e65bc63f87bcda214c529eba
tv3636 <tv3636@gmail.com>
2022-10-28 19:15:43 -0700

    Add custom metadata for new forgotten runes collection and update README with steps to add custom metadata

---
+++ src/custom/forgotten-runes-athenaeum/index.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import _ from "lodash";
+
+export const fetchToken = async (_chainId, { contract, tokenId }) => {
+  return axios
+    .get(`https://portal.forgottenrunes.com/api/treats/data/${tokenId}`)
+    .then((response) => {
+      const attributes = [{'key': 'name', 'value': response.data.name}];
+
+      return {
+        contract,
+        tokenId,
+        collection: _.toLower(contract),
+        name: response.data.name,
+        imageUrl: response.data.image,
+        flagged: false,
+        attributes,
+      };
+    });
+};
+
+export const fetchContractTokens = (_chainId, contract, continuation) => {
+  const pageSize = 1000;
+  const tokenIdRange = [0, 9999];
+
+  const minTokenId = continuation
+    ? Math.max(continuation, tokenIdRange[0])
+    : tokenIdRange[0];
+  const maxTokenId = continuation
+    ? Math.min(continuation + pageSize, tokenIdRange[1])
+    : tokenIdRange[1];
+
+  const assets = [];
+  for (let tokenId = minTokenId; tokenId <= maxTokenId; tokenId++) {
+    assets.push(fetchToken(_chainId, { contract, tokenId }));
+  }
+
+  return {
+    continuation: maxTokenId === tokenIdRange[1] ? undefined : maxTokenId + 1,
+    metadata,
+  };
+};
