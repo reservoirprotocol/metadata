d19e30a0d33dd8c4dfae7949ab6fedb05711bbdc
Idan Peleg <ipeleg@hotmail.com>
2022-12-21 13:35:27 -0500

    feat: use sound releaseFromToken query to fetch data

--- src/parsers/soundxyz.js
+++ src/parsers/soundxyz.js
@@ -1,14 +1,20 @@
-export const parse = (contract, tokenId, collection, nft) => {
+export const parse = (contract, tokenId, collection, releaseFromToken) => {
+  const isGoldenEgg = releaseFromToken.eggGame.nft.tokenId === tokenId;
+  let imageUrl = releaseFromToken.animatedCoverImage?.url ?? releaseFromToken.coverImage?.url ?? releaseFromToken.staticCoverImage?.url;
+  if (isGoldenEgg) {
+    imageUrl = releaseFromToken.eggGame.animatedGoldenEggImageOptimized?.url ?? releaseFromToken.eggGame.goldenEggImage?.url
+  }
+
   return {
     contract: contract,
     tokenId: tokenId,
     collection,
-    name: nft.release.title,
+    name: releaseFromToken.title,
     flagged: false,
-    description: nft.release.behindTheMusic,
-    imageUrl: nft.isGoldenEgg ? nft.release.eggGame.goldenEggImage.url : nft.release.coverImage.url,
-    mediaUrl: nft.release.track.revealedAudio.url,
-    attributes: (nft.openSeaMetadataAttributes || []).map((trait) => ({
+    description: releaseFromToken.behindTheMusic,
+    imageUrl,
+    mediaUrl: releaseFromToken.track.revealedAudio.url,
+    attributes: ((isGoldenEgg ? releaseFromToken.eggGame.nft.openSeaMetadataAttributes : releaseFromToken.baseMetadataAttributes) || []).map((trait) => ({
       key: trait.traitType || "property",
       value: trait.value,
       kind: typeof trait.value == "number" ? "number" : "string",
