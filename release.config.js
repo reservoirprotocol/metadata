a72a38900781215b217d31776a302c8da9c94c89
Filipe Mota <filipe@unevenlabs.com>
2023-03-09 12:32:45 +0100

    chore: setup github workflows
    Signed-off-by: Filipe Mota <filipe@unevenlabs.com>

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "dryRun": false,
+  "branches": ["pipeline"],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
+    // "@semantic-release/npm",
+    // "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "docs/CHANGELOG.md",
+          "package.json",
+          "package-lock.json"
+        ],
+        "message": "chore(release): update changelogs for ${nextRelease.version} [skip ci]"
+      }
+    ]
+  ]
+};
