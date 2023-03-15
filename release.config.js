521d025a9c05aa522f802b0e6dfd92afbc8fa02e
Filipe Mota <filipe@unevenlabs.com>
2023-03-15 15:34:37 +0100

    fix: setup semantic-version correct branch
    Signed-off-by: Filipe Mota <filipe@unevenlabs.com>

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "dryRun": false,
-  "branches": ["pipeline"],
+  "branches": ["main", "staging"],
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
