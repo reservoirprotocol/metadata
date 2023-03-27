54d54f732bc5b1c56effd680d2c9db75ca4a6837
augustfr <68298953+augustfr@users.noreply.github.com>
2023-03-27 09:23:25 -0700

    add opensea image

--- src/custom/mirage-gallery-curated/index.js
+++ src/custom/mirage-gallery-curated/index.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 import slugify from "slugify";
 
+import * as opensea from "../../fetchers/opensea";
+import { logger } from "../../shared/logger";
+
 function getProjectID(tokenId) {
   let tokenStr = tokenId.toString();
   if (tokenStr.length === 5) {
     return parseInt(tokenStr[0], 10);
   } else if (tokenStr.length === 6) {
     return parseInt(tokenStr.slice(0, 2), 10);
-  } 
+  }
 }
 
 export const fetchCollection = async (_chainId, { contract, tokenId }) => {
-  
   const projectID = getProjectID(tokenId);
 
   const url = `https://account.miragegallery.ai/curated-details.json`;
   const { data } = await axios.get(url);
 
-  const projectDetails = data.data.find(item => item.projectId === projectID);
+  const projectDetails = data.data.find((item) => item.projectId === projectID);
   const miragePayoutAddress = data.curatedPayoutAddress;
   const mirageBPS = data.curatedPayoutBPS;
   const artistBPS = data.artistPayoutBPS;
@@ -28,30 +30,36 @@ export const fetchCollection = async (_chainId, { contract, tokenId }) => {
   let royalties;
 
   if (projectDetails.secondaryArtistAddress == "") {
-    royalties = [{bps: mirageBPS, recipient: miragePayoutAddress}, {bps: artistBPS, recipient: projectDetails.artistAddress}]
+    royalties = [
+      { bps: mirageBPS, recipient: miragePayoutAddress },
+      { bps: artistBPS, recipient: projectDetails.artistAddress },
+    ];
   } else {
-    royalties = [{bps: mirageBPS, recipient: miragePayoutAddress}, {bps: artistBPS / 2, recipient: projectDetails.artistAddress}, {bps: artistBPS / 2, recipient: projectDetails.secondaryArtistAddress}]
+    royalties = [
+      { bps: mirageBPS, recipient: miragePayoutAddress },
+      { bps: artistBPS / 2, recipient: projectDetails.artistAddress },
+      { bps: artistBPS / 2, recipient: projectDetails.secondaryArtistAddress },
+    ];
   }
 
-  let extURL = projectDetails.website
+  let extURL = projectDetails.website;
   if (projectDetails.website == "") {
-    extURL = "https://miragegallery.ai/curated"
+    extURL = "https://miragegallery.ai/curated";
   }
 
   const { slug, openseaRoyalties, openseaFees, safelistRequestStatus } = await opensea
-  .fetchCollection(_chainId, { contract, tokenId })
-  .then((m) => ({
-    slug: m.slug,
-    openseaRoyalties: m.openseaRoyalties,
-    openseaFees: m.openseaFees,
-    safelistRequestStatus: m.metadata?.safelistRequestStatus,
-  }))
-  .catch(() => ({
-    slug: slugify(releaseFromToken.titleSlug, { lower: true }),
-    openseaRoyalties: [],
-    openseaFees: [],
-  }));
-
+    .fetchCollection(_chainId, { contract, tokenId })
+    .then((m) => ({
+      slug: m.slug,
+      openseaRoyalties: m.openseaRoyalties,
+      openseaFees: m.openseaFees,
+      safelistRequestStatus: m.metadata?.safelistRequestStatus,
+    }))
+    .catch(() => ({
+      slug: slugify(releaseFromToken.titleSlug, { lower: true }),
+      openseaRoyalties: [],
+      openseaFees: [],
+    }));
 
   return {
     id: `${contract}:${startTokenId}:${endTokenId}`,
@@ -74,23 +82,46 @@ export const fetchCollection = async (_chainId, { contract, tokenId }) => {
 };
 
 export const fetchToken = async (_chainId, { contract, tokenId }) => {
-  const url = `https://account.miragegallery.ai/curated-details.json`;
-  let { data } = await axios.get(url);
+  let data;
+  try {
+    const response = await axios.get(`https://account.miragegallery.ai/curated-details.json`);
+    data = response.data;
+  } catch (error) {
+    logger.error(
+      "mirage-gallery-curated-fetcher",
+      `fetchTokens get json error. chainId:${_chainId}, message:${error.message}, status:${
+        error.response?.status
+      }, data:${JSON.stringify(error.response?.data)}`
+    );
 
+    throw error;
+  }
   const projectID = getProjectID(tokenId);
-  const projectDetails = data.data.find(item => item.projectId === projectID);
+  const projectDetails = data.data.find((item) => item.projectId === projectID);
 
-  let metadataURL = projectDetails.metadata
+  let metadataURL = projectDetails.metadata;
 
-  if (metadataURL.startsWith('ipfs://')) {
-    metadataURL = metadataURL.replace('ipfs://', 'https://ipfs.io/ipfs/') + '/' + tokenId;
+  if (metadataURL.startsWith("ipfs://")) {
+    metadataURL = metadataURL.replace("ipfs://", "https://ipfs.io/ipfs/") + "/" + tokenId;
   } else {
-    metadataURL = metadataURL + '/' + tokenId;
+    metadataURL = metadataURL + "/" + tokenId;
   }
 
-  data  = await axios.get(metadataURL);
+  try {
+    data = await axios.get(metadataURL);
+  } catch (error) {
+    logger.error(
+      "mirage-gallery-curated-fetcher",
+      `fetchTokens get metadataURL error. chainId:${_chainId}, metadataURL=${metadataURL}, message:${
+        error.message
+      }, status:${error.response?.status}, data:${JSON.stringify(error.response?.data)}`
+    );
+
+    throw error;
+  }
 
   const attributes = [];
+
   for (const [key, value] of Object.entries(data.data.attributes)) {
     attributes.push({
       key,
@@ -103,12 +134,22 @@ export const fetchToken = async (_chainId, { contract, tokenId }) => {
   const startTokenId = tokenId - (tokenId % 10000);
   const endTokenId = startTokenId + 10000 - 1;
 
+  let imageUrl;
+
+  // Try to fetch image from opensea, fallback to ipfs image on failure
+  try {
+    const osData = await opensea.fetchTokens(_chainId, [{ contract, tokenId }]);
+    imageUrl = osData[0].imageUrl ?? data.data.image;
+  } catch (e) {
+    imageUrl = data.data.image;
+  }
+
   return {
     contract,
     tokenId,
     collection: `${contract}:${startTokenId}:${endTokenId}`,
-    name: projectDetails.dropName,
-    imageUrl: projectDetails.image,
+    name: data.data.name,
+    imageUrl,
     flagged: false,
     attributes,
   };
