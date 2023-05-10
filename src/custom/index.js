6348c90475ab6c35f9142162fff285207d279c9f
tv <tv3636@gmail.com>
2023-05-10 16:39:02 -0700

    fix: move custom logic to extend in progress

--- src/custom/index.js
+++ src/custom/index.js
@@ -39,26 +39,6 @@ export const customHandleContractTokens = async (chainId, contract, continuation
 
 const customCollection = {};
 
-// ArtBlocks
-customCollection["1,0x059edd72cd353df5106d2b9cc5ab83a52287ac3a"] = artblocks;
-customCollection["1,0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270"] = artblocks;
-customCollection["1,0x99a9b7c1116f9ceeb1652de04d5969cce509b069"] = artblocks;
-customCollection["5,0xda62f67be7194775a75be91cbf9feedcc5776d4b"] = artblocks;
-customCollection["5,0xb614c578062a62714c927cd8193f0b8bfb90055c"] = artblocks;
-
-// ArtBlocks Engine
-customCollection["1,0xbdde08bd57e5c9fd563ee7ac61618cb2ecdc0ce0"] = artblocksEngine;
-customCollection["1,0x28f2d3805652fb5d359486dffb7d08320d403240"] = artblocksEngine;
-customCollection["1,0x64780ce53f6e966e18a22af13a2f97369580ec11"] = artblocksEngine;
-customCollection["1,0x010be6545e14f1dc50256286d9920e833f809c6a"] = artblocksEngine;
-customCollection["1,0x13aae6f9599880edbb7d144bb13f1212cee99533"] = artblocksEngine;
-customCollection["1,0xa319c382a702682129fcbf55d514e61a16f97f9c"] = artblocksEngine;
-customCollection["1,0xd10e3dee203579fcee90ed7d0bdd8086f7e53beb"] = artblocksEngine;
-customCollection["1,0x62e37f664b5945629b6549a87f8e10ed0b6d923b"] = artblocksEngine;
-customCollection["1,0x0a1bbd57033f57e7b6743621b79fcb9eb2ce3676"] = artblocksEngine;
-customCollection["1,0x942bc2d3e7a589fe5bd4a5c6ef9727dfd82f5c8a"] = artblocksEngine;
-customCollection["1,0x32d4be5ee74376e08038d652d4dc26e62c67f436"] = artblocksEngine;
-
 // Sound XYZ
 soundxyz.SoundxyzArtistContracts.forEach(
   (address) => (customCollection[`1,${address}`] = soundxyz)
@@ -79,26 +59,6 @@ customCollection["1,0xb7ec7bbd2d2193b47027247fc666fb342d23c4b5"] = mirageGallery
 ////////////////
 
 const custom = {};
-
-// ArtBlocks
-custom["1,0x059edd72cd353df5106d2b9cc5ab83a52287ac3a"] = artblocks;
-custom["1,0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270"] = artblocks;
-custom["1,0x99a9b7c1116f9ceeb1652de04d5969cce509b069"] = artblocks;
-custom["5,0xda62f67be7194775a75be91cbf9feedcc5776d4b"] = artblocks;
-custom["5,0xb614c578062a62714c927cd8193f0b8bfb90055c"] = artblocks;
-
-// ArtBlocks Engine
-custom["1,0xbdde08bd57e5c9fd563ee7ac61618cb2ecdc0ce0"] = artblocksEngine;
-custom["1,0x28f2d3805652fb5d359486dffb7d08320d403240"] = artblocksEngine;
-custom["1,0x64780ce53f6e966e18a22af13a2f97369580ec11"] = artblocksEngine;
-custom["1,0x010be6545e14f1dc50256286d9920e833f809c6a"] = artblocksEngine;
-custom["1,0x13aae6f9599880edbb7d144bb13f1212cee99533"] = artblocksEngine;
-custom["1,0xa319c382a702682129fcbf55d514e61a16f97f9c"] = artblocksEngine;
-custom["1,0xd10e3dee203579fcee90ed7d0bdd8086f7e53beb"] = artblocksEngine;
-custom["1,0x62e37f664b5945629b6549a87f8e10ed0b6d923b"] = artblocksEngine;
-custom["1,0x0a1bbd57033f57e7b6743621b79fcb9eb2ce3676"] = artblocksEngine;
-custom["1,0x942bc2d3e7a589fe5bd4a5c6ef9727dfd82f5c8a"] = artblocksEngine;
-custom["1,0x32d4be5ee74376e08038d652d4dc26e62c67f436"] = artblocksEngine;
 
 // Cryptokicks IRL
 custom["1,0x11708dc8a3ea69020f520c81250abb191b190110"] = cryptokicksIrl;
