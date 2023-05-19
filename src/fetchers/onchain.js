1ca9e5f36cc0fd5ec7d14836ba0c17e9bdf6a910
tv <tv3636@gmail.com>
2023-05-19 12:59:28 -0700

    fix: format

--- src/fetchers/onchain.js
+++ src/fetchers/onchain.js
@@ -7,20 +7,7 @@ import { RequestWasThrottledError } from "./errors";
 
 const FETCH_TIMEOUT = 30000;
 const ALLOWED_CHAIN_IDS = [
-  1, 
-  5, 
-  10, 
-  56, 
-  137, 
-  42161, 
-  534353, 
-  5001, 
-  59140, 
-  11155111, 
-  80001, 
-  84531,
-  42170,
-  999,
+  1, 5, 10, 56, 137, 42161, 534353, 5001, 59140, 11155111, 80001, 84531, 42170, 999,
 ];
 
 const erc721Interface = new ethers.utils.Interface([
