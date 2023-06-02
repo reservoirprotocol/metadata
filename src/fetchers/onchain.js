d6fbf66e9b89e1f54534dfc7721044d3d4bb5fe5
tv <tv3636@gmail.com>
2023-06-02 15:02:05 -0700

    fix: remove console log

--- src/fetchers/onchain.js
+++ src/fetchers/onchain.js
@@ -177,7 +177,6 @@ const getTokenMetadataFromURI = async (uri) => {
 export const fetchTokens = async (chainId, tokens) => {
   // TODO: Add support for other chains via RPC_URL
   const network = _.upperCase(supportedChains[chainId]).replace(" ", "_");
-  console.log(`RPC_URL_${network}`);
 
   if (tokens.length === 0) return [];
   if (!Array.isArray(tokens)) tokens = [tokens];
